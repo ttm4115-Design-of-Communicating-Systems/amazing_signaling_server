d7b8de1114d3b4996dfdde677b9f531662813338
theodorsm <theodor@midtlien.com>
2022-04-25 17:11:54 +0200

    feat: add initial signaling server skeleton

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # amazing_signaling_server
+
+WebRTC signaling server, matching clients at home with a random office computer.
+
+## Run
+
+```
+npm install
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,108 @@
+var Server = require("socket.io").Server;
+const io = new Server(9999, {cors: {origins: ["*"]}});
+var users = {};
+
+io.on("connection", (socket) => {
+    socket.emit("message", "Hello from server");
+
+    socket.on("message", (msg) =>{
+        var data;
+        try {
+            data = JSON.parse(msg);
+        } catch (e) {
+            console.log("Invalid JSON");
+            data = {};
+        }
+        console.log(data);
+
+        //switching type of the user message 
+        switch (data.type) { 
+            //when a user tries to login 
+            case "login": 
+                console.log("User logged:", data.name); 
+                    
+                //if anyone is logged in with this username then refuse 
+                if(users[data.name]) { 
+                    sendTo(socket, { 
+                    type: "login", 
+                    success: false 
+                    }); 
+                } else { 
+                    //save user connection on the server 
+                    users[data.name] = socket; 
+                    socket.name = data.name; 
+                        
+                    sendTo(socket, { 
+                    type: "login", 
+                    success: true 
+                    });
+                        
+                } 
+                    
+                break;
+
+            // TODO: offer to random rather than name.
+            case "offer": 
+                //for ex. UserA wants to call UserB 
+                console.log("Sending offer to: ", data.name); 
+                    
+                //if UserB exists then send him offer details 
+                var conn = users[data.name]; 
+                    
+                if(conn != null){ 
+                    //setting that UserA connected with UserB 
+                    socket.otherName = data.name; 
+                        
+                    sendTo(conn, { 
+                        type: "offer", 
+                        offer: data.offer, 
+                        name: socket.name 
+                    }); 
+                }
+                    
+                break;                        
+
+            case "answer": 
+                console.log("Sending answer to: ", data.name); 
+                    
+                //for ex. UserB answers UserA 
+                var conn = users[data.name]; 
+                    
+                if(conn != null) { 
+                    socket.otherName = data.name; 
+                    sendTo(conn, { 
+                        type: "answer", 
+                        answer: data.answer 
+                    }); 
+                }
+                    
+                break;
+
+            case "candidate":
+                console.log("Sending candidate to:" data.name);
+                var conn = users[data.name];
+
+                if(conn != null) {
+                    sendTo(conn, {
+                        type: "candidate",
+                        candidate: data.candidate
+                    });
+                }
+
+                break;
+
+            default: 
+                sendTo(socket, { 
+                    type: "error", 
+                    message: "Command no found: " + data.type 
+                }); 
+                    
+                break; 
+        } 
+    })
+})
+
+
+function sendTo(socket, message) { 
+    socket.emit("message", JSON.stringify(message));
+}
