6e64c69c04ca931e940f586f1a6083454bd68683
theodorsm <theodor@midtlien.com>
2022-04-28 11:03:39 +0200

    fix: update TODO in offer

--- server.js
+++ server.js
@@ -1,106 +1,104 @@
 var Server = require("socket.io").Server;
-const io = new Server(9999, {cors: {origins: ["*"]}});
+const io = new Server(9999, { cors: { origins: ["*"] } });
 var users = {};
 
 io.on("connection", (socket) => {
-    socket.on("message", (msg) =>{
-        var data;
-        try {
-            data = JSON.parse(msg);
-        } catch (e) {
-            console.log("Invalid JSON");
-            data = {};
+  socket.on("message", (msg) => {
+    var data;
+    try {
+      data = JSON.parse(msg);
+    } catch (e) {
+      console.log("Invalid JSON");
+      data = {};
+    }
+    console.log(data);
+
+    //switching type of the user message
+    switch (data.type) {
+      //when a user tries to login
+      case "login":
+        console.log("User logged:", data.name);
+
+        //if anyone is logged in with this username then refuse
+        if (users[data.name]) {
+          sendTo(socket, {
+            type: "login",
+            success: false,
+          });
+        } else {
+          //save user connection on the server
+          users[data.name] = socket;
+          socket.name = data.name;
+
+          sendTo(socket, {
+            type: "login",
+            success: true,
+          });
         }
-        console.log(data);
 
-        //switching type of the user message
-        switch (data.type) {
-            //when a user tries to login
-            case "login":
-                console.log("User logged:", data.name);
+        break;
 
-                //if anyone is logged in with this username then refuse
-                if(users[data.name]) {
-                    sendTo(socket, {
-                    type: "login",
-                    success: false
-                    });
-                } else {
-                    //save user connection on the server
-                    users[data.name] = socket;
-                    socket.name = data.name;
+      // TODO: pick most recent active office user
+      case "offer":
+        //for ex. UserA wants to call UserB
+        console.log("Sending offer to: ", data.name);
 
-                    sendTo(socket, {
-                    type: "login",
-                    success: true
-                    });
+        //if UserB exists then send him offer details
+        var conn = users[data.name];
 
-                }
+        if (conn != null) {
+          //setting that UserA connected with UserB
+          socket.otherName = data.name;
 
-                break;
-
-            // TODO: offer to random rather than name.
-            case "offer":
-                //for ex. UserA wants to call UserB
-                console.log("Sending offer to: ", data.name);
-
-                //if UserB exists then send him offer details
-                var conn = users[data.name];
-
-                if(conn != null){
-                    //setting that UserA connected with UserB
-                    socket.otherName = data.name;
-
-                    sendTo(conn, {
-                        type: "offer",
-                        offer: data.offer,
-                        name: socket.name
-                    });
-                }
-
-                break;
-
-            case "answer":
-                console.log("Sending answer to: ", data.name);
-
-                //for ex. UserB answers UserA
-                var conn = users[data.name];
-
-                if(conn != null) {
-                    socket.otherName = data.name;
-                    sendTo(conn, {
-                        type: "answer",
-                        answer: data.answer
-                    });
-                }
-
-                break;
-
-            case "candidate":
-                console.log("Sending candidate to:" +  data.name);
-                var conn = users[data.name];
-
-                if(conn != null) {
-                    sendTo(conn, {
-                        type: "candidate",
-                        candidate: data.candidate
-                    });
-                }
-
-                break;
-
-            default:
-                sendTo(socket, {
-                    type: "error",
-                    message: "Command no found: " + data.type
-                });
-
-                break;
+          sendTo(conn, {
+            type: "offer",
+            offer: data.offer,
+            name: socket.name,
+          });
         }
-    })
-})
 
+        break;
+
+      case "answer":
+        console.log("Sending answer to: ", data.name);
+
+        //for ex. UserB answers UserA
+        var conn = users[data.name];
+
+        if (conn != null) {
+          socket.otherName = data.name;
+          sendTo(conn, {
+            type: "answer",
+            answer: data.answer,
+          });
+        }
+
+        break;
+
+      case "candidate":
+        console.log("Sending candidate to:" + data.name);
+        var conn = users[data.name];
+
+        if (conn != null) {
+          sendTo(conn, {
+            type: "candidate",
+            candidate: data.candidate,
+          });
+        }
+
+        break;
+
+      default:
+        sendTo(socket, {
+          type: "error",
+          message: "Command no found: " + data.type,
+        });
+
+        break;
+    }
+  });
+});
 
 function sendTo(socket, message) {
-    socket.emit("message", JSON.stringify(message));
+  socket.emit("message", JSON.stringify(message));
 }
